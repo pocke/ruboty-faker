8dd3a1566a5be957d7ea136266b3dcec1b24b909
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-01-18 14:26:00 +0900

    bunlde gem

---
+++ lib/ruboty/faker.rb
@@ -0,0 +1,8 @@
+require "ruboty/faker/version"
+
+module Ruboty
+  module Faker
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/faker/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Faker
+    VERSION = "0.1.0"
+  end
+end
