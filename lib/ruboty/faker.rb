150a8f92642ddc32e8e86c1926935b010cb09d88
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-01-18 14:38:10 +0900

    Implement it

--- lib/ruboty/faker.rb
+++ lib/ruboty/faker.rb
@@ -1,8 +1,28 @@
+require 'faker'
 require "ruboty/faker/version"
 
 module Ruboty
-  module Faker
-    class Error < StandardError; end
-    # Your code goes here...
+  module Handlers
+    class Faker < Base
+      on(
+        /give me (?:a )?name/i,
+        name: 'name',
+        description: 'Ruboty gives you a name',
+      )
+
+      on(
+        /give me (?:a )?quote/i,
+        name: 'quote',
+        description: 'Quote by matz',
+      )
+
+      def name(message)
+        message.reply ::Faker::Name.name
+      end
+
+      def quote(message)
+        message.reply ::Faker::Quote.matz
+      end
+    end
   end
 end
